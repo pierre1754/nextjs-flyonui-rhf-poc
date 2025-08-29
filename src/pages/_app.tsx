03d5416aa2af1928612bb3cf22767553aa4518d9
pierre.hamel <pierre1754@outlook.com>
2025-08-29 15:50:19 +0200

    feat: setup flyonui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
+import FlyonuiScript from "@/components/FlyonuiScript";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <FlyonuiScript />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <button>test</button>;
+  return <button className="btn btn-primary">Primary</button>;
 }
